24289c5a23e84c8b3daa955162f32aadbc2b0bec
BevzoI <bevzo.i.n@gmail.com>
2024-01-19 11:23:32 +0100

    upp src

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,46 @@
+import {
+    ATTRIBUTES,
+    ATTRIBUTE_METHODS,
+    handleAttributeMethods,
+  } from './common';
+  
+  const refs = {
+    startBtn: document.querySelector('button[data-start]'),
+    stopBtn: document.querySelector('button[data-stop]'),
+    body: document.querySelector('body'),
+  };
+  
+  let intervalId;
+  const getRandomHexColor = () => {
+    return `#${Math.floor(Math.random() * 16777215)
+      .toString(16)
+      .padStart(6, 0)}`;
+  };
+  
+  const handleStart = () => {
+    handleAttributeMethods(ATTRIBUTES.disabled, ATTRIBUTE_METHODS.toggle, [
+      refs.startBtn,
+      refs.stopBtn,
+    ]);
+  
+    intervalId = setInterval(() => {
+      refs.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+  
+    console.log('Active interval id:', intervalId);
+  };
+  
+  const handleStop = () => {
+    clearInterval(intervalId);
+    handleAttributeMethods(ATTRIBUTES.disabled, ATTRIBUTE_METHODS.toggle, [
+      refs.startBtn,
+      refs.stopBtn,
+    ]);
+  
+    console.log('Clear active interval id:', intervalId);
+  
+    intervalId = null;
+  };
+  
+  refs.startBtn.addEventListener('click', handleStart);
+  refs.stopBtn.addEventListener('click', handleStop);
